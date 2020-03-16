ac28f6fa57cf7bc288cf54e0495b5349400d3f5f
mirstigais <50892520+mirstigais@users.noreply.github.com>
2020-03-16 04:55:44 +0200

    Initial commit

---
+++ resources/views/layout/partials/header.blade.php
@@ -0,0 +1,4 @@
+<section class="jumbotron text-center">
+     <div class="container">
+     </div>
+</section>
---
+++ resources/views/layout/partials/nav.blade.php
@@ -0,0 +1,56 @@
+<div class="collapse bg-inverse" id="navbarHeader">
+
+     <div class="container">
+
+       <div class="row">
+
+         <div class="col-sm-8 py-4">
+
+           <h4 class="text-white">About</h4>
+         </div>
+
+         <div class="col-sm-4 py-4">
+
+           <h4 class="text-white">Contact</h4>
+
+           <ul class="list-unstyled">
+
+             <li><a href="#" class="text-white">Follow on Twitter</a></li>
+
+             <li><a href="#" class="text-white">Like on Facebook</a></li>
+
+             <li><a href="#" class="text-white">Email me</a></li>
+
+           </ul>
+
+         </div>
+
+       </div>
+
+     </div>
+
+   </div>
+
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    
